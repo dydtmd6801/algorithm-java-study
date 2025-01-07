6550e1c3439bccf181b11c22c001fbd4566acbf5
dydtmd6801 <dydtmd6801@naver.com>
2025-01-07 17:21:27 +0900

    ADD : Add TestCode
    테스트 코드 및 각 기능별 함수 작성
    각 기능에 맞춰 테스트할 수 있는 코드 작성
    테스트에 맞춰서 Solution 클래스에 코드 작성
    
    ' ----------------------------------------
    ' 타입 종류
    ' * ADD : 새로운 기능 추가
    ' * MODIFY : 기존 기능 수정
    ' * FIX : 버그 수정
    ' * DOCS : 문서 수정
    ' * CHORE : 빌드 부분 혹은 패키지 매니저 수정사항
    '
    ' 제목 규칙
    ' * 제목 작성 후 공백 한줄을 포함해야, 제목과 본문이 구별됨
    ' * 제목 첫 글자는 대문자로 작성, 마침표를 사용하지 않음
    ' * 제목은 명령문으로 사용, 과거형을 사용하지 않음
    '
    ' 본문 규칙
    ' * 본문 작성 후 공백 한줄을 포함, 본문과 Resolves가 구별
    ' * 본문의 각 행은 72글자로 제한
    ' * 본문은 "왜"와 "무엇을"위주로 작성
    ' * 본문은 행으로 구분되어야 함
    
    출처 : https://velog.io/@imkkuk/Git-Commit-Message-Template-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0Git-Commit-Convention#%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EB%B3%84-commit-template-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0

--- Lv.1/src/solution340213/Solution.java
+++ Lv.1/src/solution340213/Solution.java
@@ -2,4 +2,23 @@ package solution340213;
 
 public class Solution {
 
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        return "";
+    }
+
+    public int next10Second(int second) {
+        return 0;
+    }
+
+    public int prev10Second(int second) {
+        return 0;
+    }
+
+    public int moveVideoLength(int videoLength, int currentPosition) {
+        return 0;
+    }
+
+    public int moveOpeningEnd(int opStart, int opEnd, int currentPosition) {
+        return 0;
+    }
 }
--- Lv.1/test/solution340213/SolutionTest.java
+++ Lv.1/test/solution340213/SolutionTest.java
@@ -1,5 +1,100 @@
 package solution340213;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SolutionTest {
 
+    Solution solution;
+
+    @BeforeEach
+    void SetSolution() {solution = new Solution();}
+
+    @Test
+    @DisplayName("현재 재생구간 10초 앞으로 이동")
+    void moveNext10second() {
+        int second = 125;
+        int result = 135;
+
+        assertThat(solution.next10Second(second)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("현재 재생구간 10초 뒤로 이동")
+    void movePrev10second() {
+        int second = 43;
+        int result = 33;
+
+        assertThat(solution.prev10Second(second)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("현재 재생구간을 영상 안으로 이동")
+    void moveVideoLengthMax() {
+        int videoLength = 453;
+        int currentPosition = 448;
+        int result = 453;
+
+        assertThat(solution.moveVideoLength(videoLength, currentPosition)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("현재 재생구간을 영상 안으로 이동")
+    void moveVideoLengthMin() {
+        int videoLength = 453;
+        int currentPosition = 5;
+        int result = 0;
+
+        assertThat(solution.moveVideoLength(videoLength, currentPosition)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("현재 재생구간을 오프닝 끝으로 이동")
+    void moveOpeningEnd() {
+        int opStart = 15;
+        int opEnd = 132;
+        int currentPosition = 127;
+        int result = 132;
+
+        assertThat(solution.moveOpeningEnd(opStart, opEnd, currentPosition)).isEqualTo(result);
+    }
+
+    @Test
+    void case01() {
+        String video_len = "34:33";
+        String pos = "13:00";
+        String op_start = "00:55";
+        String op_end = "02:55";
+        String[] commands = {"next", "prev"};
+        String result = "13:00";
+
+        assertThat(solution.solution(video_len, pos, op_start, op_end, commands)).isEqualTo(result);
+    }
+
+    @Test
+    void case02() {
+        String video_len = "10:55";
+        String pos = "00:05";
+        String op_start = "00:15";
+        String op_end = "06:55";
+        String[] commands = {"prev", "next", "next"};
+        String result = "06:55";
+
+        assertThat(solution.solution(video_len, pos, op_start, op_end, commands)).isEqualTo(result);
+    }
+
+    @Test
+    void case03() {
+        String video_len = "07:22";
+        String pos = "04:05";
+        String op_start = "00:15";
+        String op_end = "04:07";
+        String[] commands = {"next"};
+        String result = "04:17";
+
+        assertThat(solution.solution(video_len, pos, op_start, op_end, commands)).isEqualTo(result);
+    }
 }
