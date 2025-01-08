4c138a2aa885339ec61a113f9ca0943683dcd5c1
dydtmd6801 <dydtmd6801@naver.com>
2025-01-08 15:01:31 +0900

    ADD : solved solution 340213
    readme와 테스트 코드를 토대로 각 기능별 코드 완성
    각 기능을 수행 후 결과값을 return하여 변수에 대입
    
    ' ----------------------------------------
    ' 타입 종류
    ' * ADD : 새로운 기능 추가
    ' * MODIFY : 기존 기능 수정
    ' * FIX : 버그 수정
    ' * DOCS : 문서 수정
    ' * CHORE : 빌드 부분 혹은 패키지 매니저 수정사항
    '
    ' 제목 규칙
    ' * 제목 작성 후 공백 한줄을 포함해야, 제목과 본문이 구별됨
    ' * 제목 첫 글자는 대문자로 작성, 마침표를 사용하지 않음
    ' * 제목은 명령문으로 사용, 과거형을 사용하지 않음
    '
    ' 본문 규칙
    ' * 본문 작성 후 공백 한줄을 포함, 본문과 Resolves가 구별
    ' * 본문의 각 행은 72글자로 제한
    ' * 본문은 "왜"와 "무엇을"위주로 작성
    ' * 본문은 행으로 구분되어야 함
    
    출처 : https://velog.io/@imkkuk/Git-Commit-Message-Template-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0Git-Commit-Convention#%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EB%B3%84-commit-template-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0

--- Lv.1/src/solution340213/Solution.java
+++ Lv.1/src/solution340213/Solution.java
@@ -3,22 +3,63 @@ package solution340213;
 public class Solution {
 
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        return "";
+        int videoLen = Integer.parseInt(video_len.substring(0,2)) * 60 + Integer.parseInt(video_len.substring(3,5));
+        int position = Integer.parseInt(pos.substring(0,2)) * 60 + Integer.parseInt(pos.substring(3,5));
+        int opStart = Integer.parseInt(op_start.substring(0,2)) * 60 + Integer.parseInt(op_start.substring(3,5));
+        int opEnd = Integer.parseInt(op_end.substring(0,2)) * 60 + Integer.parseInt(op_end.substring(3,5));
+
+        for (String command:commands) {
+            position = moveOpeningEnd(opStart, opEnd, position);
+            if (command.equals("next")) {
+                position = next10Second(position);
+            } else {
+                position = prev10Second(position);
+            }
+            position = moveVideoLength(videoLen, position);
+            position = moveOpeningEnd(opStart, opEnd, position);
+        }
+
+        int hour = position / 60;
+        int minute = position % 60;
+        String answer = "";
+        if (hour < 10) {
+            answer = answer + "0" + hour;
+        } else {
+            answer = answer + hour;
+        }
+
+        if (minute < 10) {
+            answer = answer + ":0" + minute;
+        } else {
+            answer = answer + ":" + minute;
+        }
+
+        return answer;
     }
 
     public int next10Second(int second) {
-        return 0;
+        second += 10;
+        return second;
     }
 
     public int prev10Second(int second) {
-        return 0;
+        second -= 10;
+        return second;
     }
 
     public int moveVideoLength(int videoLength, int currentPosition) {
-        return 0;
+        if (currentPosition < 0) {
+            currentPosition = 0;
+        } else if (currentPosition > videoLength) {
+            currentPosition = videoLength;
+        }
+        return currentPosition;
     }
 
     public int moveOpeningEnd(int opStart, int opEnd, int currentPosition) {
-        return 0;
+        if (currentPosition >= opStart && currentPosition < opEnd) {
+            currentPosition = opEnd;
+        }
+        return currentPosition;
     }
 }
