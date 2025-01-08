87386209216b92b5ede686b23316b538486556c7
dydtmd6801 <dydtmd6801@naver.com>
2025-01-08 16:16:47 +0900

    ADD : Add TestCode
    테스트 코드 및 각 기능별 함수 작성
    각 기능에 맞춰 테스트할 수 있는 코드 작성
    테스트에 맞춰서 Solution 클래스에 코드 작성

--- Lv.1/src/solution340199/Solution.java
+++ Lv.1/src/solution340199/Solution.java
@@ -1,4 +1,16 @@
 package solution340199;
 
 public class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        return answer;
+    }
+
+    public int[] foldInHalf(int[] bill) {
+        return new int[0];
+    }
+
+    public boolean billInWallet(int[] wallet, int[] bill) {
+        return false;
+    }
 }
--- Lv.1/test/solution340199/SolutionTest.java
+++ Lv.1/test/solution340199/SolutionTest.java
@@ -1,4 +1,49 @@
 package solution340199;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+
 public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setSolution() {
+        solution = new Solution();
+    }
+
+    @Test
+    @DisplayName("긴 쪽을 반틈 접는 기능")
+    void foldInHalf() {
+        int[] bill = {150,234};
+        int[] result = {150, 117};
+
+        assertThat(solution.foldInHalf(bill)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("지폐가 지갑에 들어가는지 판단 기능")
+    void checkBillInWallet() {
+        int[] wallet = {32, 45};
+        int[] bill = {43, 31};
+
+        assertThat(solution.billInWallet(wallet, bill)).isTrue();
+    }
+
+    @Test
+    void exam() {
+        int[][] wallet = {{30, 15}, {50, 50}};
+        int[][] bill = {{26, 17}, {100, 241}};
+        int[] result = {1, 4};
+
+        assertAll(
+                () -> assertThat(solution.solution(wallet[0], bill[0])).isEqualTo(result[0]),
+                () -> assertThat(solution.solution(wallet[1], bill[1])).isEqualTo(result[1])
+        );
+    }
 }
