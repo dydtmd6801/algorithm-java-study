54def8f1cde093cf1ec04167d2f2bfbef30a0558
dydtmd6801 <dydtmd6801@naver.com>
2025-01-09 19:41:46 +0900

    ADD : Solved solution 340199
    readme에 정리한 풀이와 테스트 코드를 기반으로 각 기능 완성

--- Lv.1/src/solution340199/Solution.java
+++ Lv.1/src/solution340199/Solution.java
@@ -3,14 +3,32 @@ package solution340199;
 public class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
+        while (true) {
+            if (billInWallet(wallet, bill)) {
+                break;
+            }
+            answer++;
+            bill = foldInHalf(bill);
+        }
         return answer;
     }
 
     public int[] foldInHalf(int[] bill) {
-        return new int[0];
+        if (bill[0] >= bill[1]) {
+            bill[0] = bill[0] / 2;
+        } else {
+            bill[1] = bill[1] / 2;
+        }
+        return bill;
     }
 
     public boolean billInWallet(int[] wallet, int[] bill) {
+        if (wallet[0] >= bill[0] && wallet[1] >= bill[1]) {
+            return true;
+        }
+        if (wallet[0] >= bill[1] && wallet[1] >= bill[0]) {
+            return true;
+        }
         return false;
     }
 }
