e88271452d3e7664661b98b3c308fe18117e64ac
dydtmd6801 <dydtmd6801@naver.com>
2025-01-10 22:52:29 +0900

    ADD : Add test code
    readme에 작성한 기능을 기반으로 테스트 코드 및 메소드 작성
    1. 문제에서 제공하는 테스트 케이스
    2. 매트가 park에 들어갈 경우
    3. 매트가 park에 들어가지 않는 경우

--- Lv.1/src/solution340198/Solution.java
+++ Lv.1/src/solution340198/Solution.java
@@ -1,4 +1,11 @@
 package solution340198;
 
 public class Solution {
+    public int solution(int[] mats, String[][] park) {
+        return 0;
+    }
+
+    public boolean searchMats(String[][] park, int x, int y, int mat) {
+        return false;
+    }
 }
--- Lv.1/test/solution340198/SolutionTest.java
+++ Lv.1/test/solution340198/SolutionTest.java
@@ -1,4 +1,63 @@
 package solution340198;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setSolution() {
+        solution = new Solution();
+    }
+
+    @Test
+    void test() {
+        int[] mats = {5, 3, 2};
+        String[][] park = {{"A", "A", "-1", "B", "B", "B", "B", "-1"},
+                {"A", "A", "-1", "B", "B", "B", "B", "-1"},
+                {"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"},
+                {"D", "D", "-1", "-1", "-1", "-1", "E", "-1"},
+                {"D", "D", "-1", "-1", "-1", "-1", "-1", "F"},
+                {"D", "D", "-1", "-1", "-1", "-1", "E", "-1"}};
+        int result = 3;
+
+        assertThat(solution.solution(mats, park)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("매트가 park에 들어가는지 체크하는 기능")
+    void checkMatInParkTrue() {
+        String[][] park = {
+                {"A", "A", "-1", "B","-1"},
+                {"A", "A", "-1", "B","-1"},
+                {"-1", "-1", "-1", "B","-1"},
+                {"-1", "-1", "-1", "-1","-1"},
+                {"-1", "-1", "-1", "C","C"}};
+        int x = 2;
+        int y = 0;
+        int mat = 3;
+
+        assertThat(solution.searchMats(park, x, y, mat)).isTrue();
+    }
+
+    @Test
+    @DisplayName("매트가 park에 들어가는지 체크하는 기능")
+    void checkMatInParkFalse() {
+        String[][] park = {
+                {"A", "A", "-1", "B","-1"},
+                {"A", "A", "-1", "B","-1"},
+                {"-1", "-1", "-1", "B","-1"},
+                {"-1", "-1", "-1", "-1","-1"},
+                {"-1", "-1", "-1", "C","C"}};
+        int x = 2;
+        int y = 1;
+        int mat = 3;
+
+        assertThat(solution.searchMats(park, x, y, mat)).isFalse();
+    }
 }
