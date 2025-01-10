6826f670007dd635d31e3d1b96d6c8bea0d77862
dydtmd6801 <dydtmd6801@naver.com>
2025-01-10 23:48:53 +0900

    ADD : Solved solution 340198
    340198 문제 해결
    핵심 기능 : 매트 크기만큼 반복하면서 -1이 아닌 알파벳이 나올 경우 false, 아니면 true를 return해서 매트가 park에 들어갈 수 있는지 확인

--- Lv.1/src/solution340198/Solution.java
+++ Lv.1/src/solution340198/Solution.java
@@ -1,11 +1,32 @@
 package solution340198;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Solution {
     public int solution(int[] mats, String[][] park) {
-        return 0;
+        Integer[] arr3 = Arrays.stream(mats).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr3, Collections.reverseOrder());
+        for (int mat : arr3) {
+            for (int i = 0; i < park.length - mat + 1; i++) {
+                for (int j = 0; j < park[0].length - mat + 1; j++) {
+                    if (searchMats(park, i, j, mat)) {
+                        return mat;
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
-    public boolean searchMats(String[][] park, int x, int y, int mat) {
-        return false;
+    public boolean searchMats(String[][] park ,int x, int y, int mat) {
+        for (int i = 0; i < mat; i++) {
+            for (int j = 0; j < mat; j++) {
+                if (!park[x+i][y+j].equals("-1")) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
