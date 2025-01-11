fca126f134e3b0a203464149c906ae5ba7bb9277
dydtmd6801 <dydtmd6801@naver.com>
2025-01-11 22:52:07 +0900

    ADD : Solved solution 250137
    각 기능 별 메소드를 사용하여 메인로직 추가
    1타임부터 마지막 타임까지 반복하면서
    공격 여부 체크 후, 상황에 맞게 기능하게 완성해서 해결

--- Lv.1/src/solution250137/Solution.java
+++ Lv.1/src/solution250137/Solution.java
@@ -2,7 +2,31 @@ package solution250137;
 
 public class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        return 0;
+        int attackCount = 0;
+        int bandageCount = 0;
+        int maxHealth = health;
+        int lastAttackTime = checkLastAttackTime(attacks);
+        for (int time = 1; time <= lastAttackTime; time++) {
+            if (attacks[attackCount][0] == time) {
+                health = attack(health, attacks[attackCount][1]);
+                if (isDie(health)) {
+                    return -1;
+                }
+                attackCount++;
+                bandageCount = 0;
+            } else {
+                health = bandage(health, bandage[1]);
+                bandageCount++;
+                if (bandageCount == bandage[0]) {
+                    health = bandage(health, bandage[2]);
+                    bandageCount = 0;
+                }
+                if (health > maxHealth) {
+                    health = maxHealth;
+                }
+            }
+        }
+        return health;
     }
 
     public int checkLastAttackTime(int[][] attacks) {
@@ -18,7 +42,7 @@ public class Solution {
     }
 
     public boolean isDie(int health) {
-        if (health < 0) {
+        if (health <= 0) {
             return true;
         }
         return false;
