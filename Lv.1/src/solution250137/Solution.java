6bcc07511c58dcfd3478fb5ac91c842d44d147b6
dydtmd6801 <dydtmd6801@naver.com>
2025-01-11 22:05:52 +0900

    ADD : Add Method checkLastAttackTime
    마지막 공격이 언제인지 확인하는 기능 추가
    배열의 마지막 요소의 첫번째 요소 return

--- Lv.1/src/solution250137/Solution.java
+++ Lv.1/src/solution250137/Solution.java
@@ -1,12 +1,12 @@
 package solution250137;
 
 public class Solution {
-    public int solution(int[] ints, int health, int[][] attack) {
+    public int solution(int[] bandage, int health, int[][] attacks) {
         return 0;
     }
 
     public int checkLastAttackTime(int[][] attacks) {
-        return 0;
+        return attacks[attacks.length - 1][0];
     }
 
     public int attack(int health, int damage) {
